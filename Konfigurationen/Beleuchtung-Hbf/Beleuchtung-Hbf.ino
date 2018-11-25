768d3a42ce63e676a2317eee4f7fe0fdf4b5eb4c
C.B.Ries <mail@christianbenjaminries.de>
2018-11-25 17:45:52 +0100

    general update

--- Konfigurationen/Beleuchtung-Hbf/Beleuchtung-Hbf.ino
+++ Konfigurationen/Beleuchtung-Hbf/Beleuchtung-Hbf.ino
@@ -10,7 +10,11 @@
 void setup()
 {
   randomSeed(analogRead(0));
+  delay(100);
+  Serial.begin(9600);  
+  delay(100);
   Tlc.init();
+  delay(100);
 }
 
 const int randomPortsN = 11;
@@ -31,21 +35,24 @@ void loop()
   for(int i = 0; i < randomPortsN; ++i)
     randomPorts_[i] = randomPorts[i];
     
-  int j = random(1, randomPortsN / 2);
-  for(int jj = 0; jj < j; ++jj)
+  if(runLoopCount > 10)
   {
-    int i = random(0, randomPortsN);
-    int v = randomPorts_[i];
-    if(v == -1)
-      continue;
-    Tlc.set(v, 2000);
-    randomPorts_[i] = -1;
-  }
-  Tlc.update();
-
-  if(runLoopCount > 5)
-  {
-    int waitFor = 10000 + (random(5, 20) * 1000);
+    int j = random(1, randomPortsN / 2);
+    for(int jj = 0; jj < j; ++jj)
+    {
+      int i = random(0, randomPortsN);
+      int v = randomPorts_[i];
+      if(v == -1) continue;
+      Tlc.set(v, 2000);
+      Serial.print("i: "); Serial.print(i);
+      Serial.print("  v: "); Serial.println(v);
+      randomPorts_[i] = -1;
+    }
+    Tlc.update();
+    
+    int waitFor = /*10000 +*/ (random(5, 5) * 1000);
+    Serial.print("Wait for: ");
+    Serial.println(waitFor);
     delay(waitFor);
   } 
   else
