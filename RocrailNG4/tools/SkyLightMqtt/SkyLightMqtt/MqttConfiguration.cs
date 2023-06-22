301576dfa58ac93381a814d3166e4ec9c6639424
Christian Benjamin Ries <c.ries@beckhoff.com>
2023-06-22 17:44:36 +0200

    keep backups

---
+++ RocrailNG4/tools/SkyLightMqtt/SkyLightMqtt/MqttConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace SkyLightMqtt
+{
+    internal class MqttConfiguration
+    {
+        public bool Enabled { get; set; } = false;
+        public string BrokerAddress { get; set; }
+
+        public int DefaultRed { get; set; } = 255;
+        public int DefaultGreen { get; set; } = 255;
+        public int DefaultBlue { get; set; } = 255;
+        public int DefaultWhite { get; set; } = 1023;
+    }
+}
